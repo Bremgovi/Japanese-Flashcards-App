bd0a5280d1e0903107f5a137160d4d04fb76a112
Bremgovi <bremgovidev@gmail.com>
2024-03-22 01:49:47 -0600

    Make categories and homepage elements

--- App.js
+++ App.js
@@ -1,20 +1,167 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useRef } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image, ScrollView, Button, TouchableOpacity } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useFonts, Inter_900Black, Inter_500Medium } from "@expo-google-fonts/inter";
+import CategoryList from "./components/categories";
+import NavBar from "./components/navbar";
+import { Dimensions } from "react-native";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Inter_900Black,
+    Inter_500Medium,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="HomeScreen" component={HomeScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="About" component={About} options={{ headerShown: false }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const HomeScreen = ({ navigation }) => {
+  const [dialoguePosition, setDialoguePosition] = useState({ x: 0, y: 0 });
+  const [imageLayout, setImageLayout] = useState({ width: 0, height: 0 });
+  const [isClicked, setIsClicked] = useState(false);
+  const timeoutRef = useRef(null);
+
+  const handleImageLayout = (event) => {
+    const { x, y, width, height } = event.nativeEvent.layout;
+    setImageLayout({ x, y, width, height });
+  };
+
+  const handleImageClick = () => {
+    setIsClicked(true);
+    const randomXOffset = Math.floor(Math.random() * 100) - 50;
+    const randomYOffset = Math.floor(Math.random() * 100) - 50;
+    const randomX = imageLayout.x + randomXOffset;
+    const randomY = imageLayout.y + randomYOffset;
+    setDialoguePosition({ x: randomX + 100, y: randomY + 100 });
+    clearTimeout(timeoutRef.current);
+    timeoutRef.current = setTimeout(() => {
+      setIsClicked(false);
+    }, 3000);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <NavBar title="Aprende Japonés!" subtitle="日本語を勉強します" />
+      <View style={styles.content}>
+        <TouchableOpacity onPress={handleImageClick}>
+          <Image source={require("./assets/catStudy.png")} style={styles.image} onLayout={handleImageLayout} />
+        </TouchableOpacity>
+        {isClicked && <DialogueBalloon position={dialoguePosition} />}
+        <ScrollViewHeader />
+        <ScrollView style={styles.scrollView}>
+          <CategoryList navigation={navigation} />
+        </ScrollView>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
-}
+};
+
+const About = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <NavBar />
+      <View style={styles.content}>
+        <Button title="Go to home" onPress={() => navigation.navigate("HomeScreen")} />
+      </View>
+    </View>
+  );
+};
+
+const ScrollViewHeader = () => {
+  return (
+    <View style={styles.scrollViewHeader}>
+      <Text style={styles.header}>Categorias</Text>
+    </View>
+  );
+};
+
+const DialogueBalloon = ({ position }) => {
+  const messages = [
+    "(╯°□°）╯︵ ┻━┻",
+    "😡😡😡😡😡",
+    "😾😾😾😾😾",
+    "💢💢💢",
+    "💢",
+    "ಠ_ಠ",
+    "ヽ(`Д´)ﾉ",
+    "（；¬＿¬)",
+    "(ノಠ益ಠ)ノ彡┻━┻",
+    "ヽ(｀⌒´メ)ノ",
+    "(눈_눈)",
+    "凸(-_-)凸",
+    "(¬_¬)",
+    "╰(°□°)╯",
+    "¡No me toques!",
+    "¡Déjame estudiar!",
+    "¡No molestes!",
+    "¡Vete!",
+    "¡No tengo tiempo para esto!",
+  ];
+  const randomMessage = messages[Math.floor(Math.random() * messages.length)];
+  return (
+    <View style={[styles.dialogueBalloon, { top: position.y, left: position.x }]}>
+      <Text>{randomMessage}</Text>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  header: {
+    fontFamily: "Inter_500Medium",
+    fontSize: 35,
+    letterSpacing: 5,
+    color: "white",
+    fontWeight: "bold",
+  },
+  scrollViewHeader: {
+    width: Dimensions.get("window").width,
+    backgroundColor: "rgba(21, 21, 36, 1)",
+    height: "10%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  content: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  image: {
+    width: 200,
+    height: 200,
+    marginBottom: 20,
+  },
+  dialogueBalloon: {
+    position: "absolute",
+    textAlign: "center",
+    alignItems: "center",
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 5,
+    elevation: 5,
+  },
+  scrollView: {
+    backgroundColor: "rgba(40, 40, 73, 1)",
   },
 });
---
+++ components/categories.js
@@ -0,0 +1,67 @@
+import { StyleSheet, Text, View, Image, ToastAndroid, TouchableOpacity } from "react-native";
+
+export default function CategoryList({ navigation }) {
+  const showToast = (text) => {
+    ToastAndroid.show(text + " Clicked", ToastAndroid.SHORT);
+    navigation.navigate("About");
+  };
+  const categories = [
+    { id: 1, image: require(`../assets/animales.png`), text: "Animales" },
+    { id: 2, image: require(`../assets/familia.png`), text: "Familia" },
+    { id: 3, image: require(`../assets/comida.png`), text: "Comida" },
+    { id: 4, image: require(`../assets/numeros.png`), text: "Numeros" },
+    { id: 5, image: require(`../assets/fechas.png`), text: "Fechas" },
+    { id: 6, image: require(`../assets/cultura.png`), text: "Cultura" },
+    { id: 7, image: require(`../assets/gramatica.png`), text: "Gramática" },
+    { id: 8, image: require(`../assets/verbos.png`), text: "Verbos" },
+    { id: 9, image: require(`../assets/adjetivos.png`), text: "Adjetivos" },
+  ];
+
+  return (
+    <View style={styles.categoryList}>
+      {categories.map((category) => (
+        <TouchableOpacity
+          key={category.id}
+          style={styles.categoryItem}
+          onPress={() => {
+            showToast(category.text);
+          }}
+        >
+          <Image source={category.image} style={styles.categoryImage} />
+          <Text style={styles.categoryText}>{category.text}</Text>
+        </TouchableOpacity>
+      ))}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  categoryList: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-around",
+  },
+  categoryItem: {
+    backgroundColor: "#d22a6d",
+    borderRadius: 20,
+    padding: 10,
+    width: "40%",
+    aspectRatio: 1,
+    margin: 10,
+    alignItems: "center",
+    resizeMode: "contain",
+  },
+  categoryImage: {
+    flex: 1,
+    width: "100%",
+    resizeMode: "contain",
+  },
+  categoryText: {
+    fontFamily: "Inter_500Medium",
+    fontSize: 20,
+    letterSpacing: 2,
+    fontWeight: "bold",
+    color: "white",
+    textAlign: "center",
+  },
+});
